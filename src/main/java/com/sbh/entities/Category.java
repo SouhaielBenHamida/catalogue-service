3c7861aecde6efbd8620a6a6df1da0850ee69cce
ben hamida <ben.hamida.souhaiel@gmail.com>
2019-03-27 20:53:08 +0100

    fixe JWTAuthorizationFilter

--- src/main/java/com/sbh/entities/Category.java
+++ src/main/java/com/sbh/entities/Category.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+
 
 @Document
 @Data @AllArgsConstructor @NoArgsConstructor 
--- src/main/java/com/sbh/entities/Product.java
+++ src/main/java/com/sbh/entities/Product.java
@@ -1,7 +1,5 @@
 package com.sbh.entities;
 
-import java.util.Collection;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,7 +7,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Document
 @Data @AllArgsConstructor @NoArgsConstructor 
